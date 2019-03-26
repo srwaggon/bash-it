0795d418be6f3c581bcbf45a75eb49c4e90292d1
Samuel Waggoner <samuel.waggoner@gmail.com>
2019-03-26 11:24:31 -0700

    Add octave syntax

--- custom/emacs/emacs_config.el
+++ custom/emacs/emacs_config.el
@@ -1,3 +1,12 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(load "~/.emacs.d/octave_config.el")
+
 ;; Long live spaces
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
--- custom/install_emacs.bash
+++ custom/install_emacs.bash
@@ -11,12 +11,26 @@ function install_emacs {
 }
 
 function link_emacs {
-  link=~/.emacs
-  sourcefile=$BASH_IT/custom/emacs/emacs_config.ss
-  if [ ! -e $link -a ! -h $link ]; then
-      ln -sf $sourcefile $link
+  source_file=$BASH_IT/custom/emacs/emacs_config.el
+  dest_file=~/.emacs
+  create_link $source_file $dest_file
+}
+
+function link_octave_config {
+  filename=octave_config.el
+  source_file=$BASH_IT/custom/emacs/$filename
+  dest_file=~/.emacs.d/$filename
+  create_link $source_file $dest_file
+}
+
+function create_link {
+  source_file=$1
+  dest_file=$2
+  if [ ! -e $dest_file -a ! -h $dest_file ]; then
+    ln -sf $source_file $dest_file
   fi
 }
 
 install_emacs
 link_emacs
+link_octave_config
