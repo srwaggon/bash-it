29d1e8b04a64ad63206a7a82107510b25c6ffd66
Samuel Waggoner <samuel.waggoner@gmail.com>
2019-03-25 18:15:28 -0700

    Improve files, including autoconfiguring global git configuration

--- custom/install_gitconfig.bash
+++ custom/install_gitconfig.bash
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
+git_config_file=~/.gitconfig
+
 function prompt_gitconfig {
   while true; do
     read -p "Do you wish to link .gitconfig? " yn
     case $yn in
-      [Yy]* ) link_gitconfig; break;;
+      [Yy]* ) copy_gitconfig; break;;
       [Nn]* ) break;;
       * ) echo "Please answer yes or no.";;
     esac
   done
 }
 
-function link_gitconfig {
-  link=~/.gitconfig
+function copy_gitconfig {
   source_file=$BASH_IT/custom/git/gitconfig
-  ln -sf $link $source_file
-  echo "Linked $sourceFile --> $link"
+  ln -sf $source_file $git_config_file
+  echo "Linked $git_config_file --> $source_file"
 }
 
-prompt_gitconfig
+
+if [ ! -f $git_config_file -a ! -h $git_config_file ]; then
+  prompt_gitconfig
+fi
