7dcffb11c5cbf7e9a2d2487d044f20d4612782d9
Samuel Waggoner <samuel.waggoner@gmail.com>
2019-07-16 21:47:40 -0700

    Add update_apt.bash to keep apt packages updated.

---
+++ custom/update_apt.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo Updating apt...
+
+sudo apt dist-upgrade \
+     --assume-yes \
+     --quiet
+
+sudo apt autoremove \
+     --assume-yes \
+     --quiet
+
+echo Updating apt... complete.
+
