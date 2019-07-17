3b5633a89261dd9346805229d01213c2eba32ade
Samuel Waggoner <samuel.waggoner@gmail.com>
2019-07-16 22:31:59 -0700

    Add java installer

---
+++ custom/install_java.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function install_java() {
+  java --version >> /dev/null
+  if [ $? -ne 0 ]; then
+    echo Installing Java...
+    sudo apt-get install openjdk-11-jdk-headless
+    echo Installing Java... complete.
+  fi
+}
+
+install_java
